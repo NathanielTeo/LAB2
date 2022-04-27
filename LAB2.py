0a2dcd4bebf2640966c440be7b89cbed9c5e8258
Nathaniel Teo <NATHANIELTRY26.21@ichat.sp.edu.sg>
2022-04-27 14:51:28 +0800

    ver 1.1

--- LAB2.py
+++ LAB2.py
@@ -1,1 +1,34 @@
-print("ET0735")
+def main():
+    display_main_menu()
+    n=get_user_inputs()
+    avg=calc_average_temperature(n)
+    print(avg)
+
+
+def display_main_menu():
+    print ("Enter some numbers seperated by commas ")
+
+def get_user_inputs():
+    x = input()
+    y=(x.split(','))
+    y2=[]
+    for x in range(len(y)):
+        floatnum=float(y2[x])
+        y2.append(floatnum)
+    return y2
+
+def calc_average_temperature(n):
+    for num in range(len(n)):
+        sum=sum+n[num]
+
+    avg=sum/(len(n))
+    return avg
+
+
+
+
+
+
+
+if __name__ == "__main__" :
+    main()
